412e70deda6a5be30224338b6f389d472c8a7d0f
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-08-13 13:05:05 +0000

    [break]2-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  resources :books do
+    collection do
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+end 
