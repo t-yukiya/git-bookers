ff7f4ae812cbd04408938db865dd066309033c17
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-08-14 13:55:09 +0000

    [Finish]2-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,49 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to  book_path(book.id)
-  end
-
+  end 
+  
   def index
-    @books = Book.all
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to  book_path(@book.id)
+    else
+      @books = Book.all.order(id: :asc)
+      render :index
+    end 
+  end
+  
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to  book_path(@book.id)
+    else
+      render :edit
+    end 
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
